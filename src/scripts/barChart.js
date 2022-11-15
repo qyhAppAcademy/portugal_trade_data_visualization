667fe7b5a3bb1f8528684f1f539646c6bca7719e
Qiao Yang Han <qyhwork@gmail.com>
2022-11-15 00:30:35 -0500

    Made small improvement on the data visualization

--- src/scripts/barChart.js
+++ src/scripts/barChart.js
@@ -23,11 +23,26 @@ export function getBarChart(range) {
         });
         console.log(data2);
         const data3 = data2.map(d => {
-            return { Country: d["Partner Name"], Value: d["Export (US$ Thousand)"]};
+            return { Country: d["Partner Name"], Value: parseFloat(d["Export (US$ Thousand)"])};
         });
+
+        data3.sort(function compareFn(a, b) {
+            if (a.Value < b.Value) {
+                return -1;
+            }
+            else if (a.Value > b.Value) {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        });
+
+        const data4 = data3.length > 10 ? data3.slice(0, 11) : data3;
+        console.log(data4);
         // Add X axis
         const x = d3.scaleLinear()
-            .domain([0, 13000])
+            .domain([0, range[1]])
             .range([0, width]);
         svg.append("g")
             .attr("transform", `translate(0, ${height})`)
@@ -39,14 +54,14 @@ export function getBarChart(range) {
         // Y axis
         const y = d3.scaleBand()
             .range([0, height])
-            .domain(data3.map(d => d.Country))
+            .domain(data4.map(d => d.Country))
             .padding(.2);
         svg.append("g")
             .call(d3.axisLeft(y))
 
         //Bars
         svg.selectAll("myRect")
-            .data(data3)
+            .data(data4)
             .join("rect")
             .attr("x", x(0))
             .attr("y", d => y(d.Country))
