ec247edb80255ed4a1a8bae1afdd1e9983131ed0
Qiao Yang Han <qyhwork@gmail.com>
2022-11-14 20:34:27 -0500

    Updated the bar chart with scaling to fit functionality

--- src/scripts/barChart.js
+++ src/scripts/barChart.js
@@ -1,12 +1,16 @@
 export function getBarChart() {
     const data = [4, 8, 15, 16, 23, 42];
 
+    const x = d3.scaleLinear()
+        .domain([0, d3.max(data)])
+        .range([0, 420]);
+
     d3.select("#bar-chart")
         .data(data)
         .join("div")
         .style("background", "steelblue")
         .style("padding", "3px")
         .style("margin", "1px")
-        .style("width", d => `${d * 10}px`)
+        .style("width", d => `${x(d)}px`)
         .text(d => d);
 }
