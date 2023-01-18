923cd895ba9f83559685b63bcc0403794950a952
Qiao Yang Han <qyhwork@gmail.com>
2023-01-17 19:28:43 -0500

    Worked on deployment

--- src/index.js
+++ src/index.js
@@ -13,9 +13,12 @@ document.addEventListener("DOMContentLoaded", () => {
         event.preventDefault();
         
         toggleBtn.children[0].children[0].innerText = world.tradeURL === EXPORT_URL ? "To Exports" : "To Imports";
-        document.getElementById("project-title").innerText = world.tradeURL === EXPORT_URL ? "Portugal Exports Data Visualization" : "Portugal Imports Data Visualization";
+
         reset();
-        
+
+        const projectTitle = document.getElementById("project-title");
+        projectTitle.innerText = world.tradeURL === EXPORT_URL ? "Portugal Imports Data Visualization" : "Portugal Exports Data Visualization";
+
         world = world.tradeURL === EXPORT_URL ? new World(IMPORT_URL) : new World(EXPORT_URL);
         world.render();
 
